56b5d4eeea461f9c2b2827fdc47f5a257270a530
senaCeejay <senaceejay2425@gmail.com>
2022-10-26 11:51:54 +0800

    pushing darkmode script

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// image animation
 const items = document.querySelectorAll('.fadeIn');
 
 const active = function (entries) {
@@ -14,7 +15,7 @@ for (let i = 0; i < items.length; i++) {
     io2.observe(items[i]);
 }
 
-
+//text animation
 const txt = document.querySelectorAll('.txtfadeIn');
 
 const act = function (entries) {
@@ -30,3 +31,12 @@ const io3 = new IntersectionObserver(act);
 for (let i = 0; i < txt.length; i++) {
     io3.observe(txt[i]);
 }
+
+//dark mode
+const scrollbind = el => el.bind("scroll", function () {
+    scroll = $(this).scrollTop();
+    if ($(".container").length > 1)
+        $(".container").scrollTop(scroll);
+
+});
+scrollbind($(".container"));
